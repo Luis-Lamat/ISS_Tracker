c13bd7b600f988c3c89a11ce0e381d3ff4122f23
Luis-Lamat <beto_alaska@hotmail.com>
2014-10-20 01:45:09 -0500

    Added geocoder and google maps link to iss address

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,14 @@
 require 'rubygems'
 require 'HTTParty'
+require 'Geocoder'
 
 class MainController < ApplicationController
 
   def index
     info  = HTTParty.get('http://api.open-notify.org/iss-now.json')
-    @lat = info['iss_position']['latitude']
-    @lon = info['iss_position']['longitude']
+    @lat  = info['iss_position']['latitude'].round(6)
+    @lon  = info['iss_position']['longitude'].round(6)
+    @data = Geocoder.address("#{@lat}, #{@lon}")
   end
 
 end
