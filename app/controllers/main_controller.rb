b81dab55d99c1a926af63b442380f13d72cb192d
Luis-Lamat <beto_alaska@hotmail.com>
2014-10-20 00:31:00 -0500

    Added JSON info to main view

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,12 @@
+require 'rubygems'
+require 'HTTParty'
+
 class MainController < ApplicationController
 
   def index
-    
+    info  = HTTParty.get('http://api.open-notify.org/iss-now.json')
+    @lat = info['iss_position']['latitude']
+    @lon = info['iss_position']['longitude']
   end
 
 end
